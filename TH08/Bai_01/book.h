dafc7e0407c1f01317f68bc1461ffb5060f9019b
t-rang <tranhuyentrang1806@gmail.com>
2020-12-23 22:24:10 +0700

    Bai_01_book.h

---
+++ TH08/Bai_01/book.h
@@ -0,0 +1,40 @@
+#ifndef TH08_BOOK_H
+#define TH08_BOOK_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class book
+{
+protected:
+    string title;
+    int year;
+    int type;
+public:
+    book(){};
+    book(book& b)
+    {
+        title = b.title;
+        year = b.year;
+        type = b.type;
+    };
+    virtual void input()
+    {
+        cout << "Title: ";
+        cin.ignore();
+        getline(cin, title);
+        cout << "Published in: ";
+        cin >> year;
+    };
+    virtual void input()
+    {
+        cout << "Title: " << title;
+        cout << "Published in: " << year;
+    };
+    virtual int getType()
+    {
+        return 99;
+    }
+    ~book(){};
+};
+#endif //TH08_BOOK_H
---
+++ TH08/Bai_01/main.cpp
@@ -0,0 +1,9 @@
+// Xây dựng các loại đối tượng sách, sách giáo khoa, tiểu thuyết, tạp chí.
+// Viết chương trình cho phép quản lý một danh sách các loại đối tượng kể trên.
+
+// book (title, year, type / input, output)
+// textbook (subject / input, output)
+// novel (author, fictional / input, output)
+// magazine (theme / input, output)
+// bookstore (n, vector / input, output, countType, add, remove)
+
